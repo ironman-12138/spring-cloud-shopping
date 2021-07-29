d18e4099ac64389ef2b782c914d785802cdbe210
xcoder <1481806085@163.com>
2021-07-29 10:56:20 +0800

    Xqq 2021年8月4日 11点02分

---
+++ shopping-parent/shopping-service/shopping-service-consumer/src/main/java/com/shopping/config/ApplicationContextConfig.java
@@ -0,0 +1,17 @@
+package com.shopping.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationContextConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getResTemplate(){
+        return new RestTemplate();
+    }
+
+}
